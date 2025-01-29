f200152bb64a4bb83bc801850daf385f81ad9f3b
Sumit Tripathi <sumit.tripathi@edmissioncrm.com>
2025-01-29 20:56:48 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import CartList from "./pages/CartList";
+import ProductList from "./pages/ProductList";
+import ProductDetails from "./pages/ProductDetails";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <h1>HII</h1>
+      <Routes>
+        <Route path="/cart" element={<CartList />} />
+        <Route path="/product-list" element={<ProductList />} />
+        <Route path="/product-details/:id" element={<ProductDetails />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }
