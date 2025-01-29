6c04017f7998d603e06b65f707da835fda4a2167
Sumit Tripathi <sumit.tripathi@edmissioncrm.com>
2025-01-29 20:56:25 +0530

    Product Card and details updated

---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,35 @@
+import { useContext } from "react";
+import { ShoppingCartContext } from "../context";
+import ProductTile from "../components/ProductTile";
+
+function ProductList() {
+  const { productList, loading } = useContext(ShoppingCartContext);
+
+  if (loading) return <h1>Loading data! Please wait.</h1>;
+
+  return (
+    <section className="py-12 bg-white sm:py-16 lg:py-20">
+      <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+        <div className="max-w-md mx-auto text-center">
+          <h2 className="text-3xl font-extralight text-gray-950 sm:text-4xl">
+            Our Products
+          </h2>
+        </div>
+        <div className="grid grid-cols-2 gap-5 mt-10 lg:mt-16 lg:gap-8 lg:grid-cols-4">
+          {productList && productList.length > 0 ? (
+            productList.map((singleProductTile) => (
+              <ProductTile
+                key={singleProductTile.id}
+                singleProductTile={singleProductTile}
+              />
+            ))
+          ) : (
+            <h3>No Products Found</h3>
+          )}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default ProductList;
